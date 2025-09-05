d9cbc3803f47620cd54dbeec1b6badd653c820f2
gorgick <gorgick2014@yandex.by>
2025-09-05 19:33:34 +0300

    detail

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, product_detail
 
 app_name = 'product'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<str:pk>/', product_detail, name='detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -5,3 +5,8 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'product/base.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product/product_detail.html', {'product': product})
