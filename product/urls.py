84437745b8c281857e6cddda644163a66b1cd822
gorgick <gorgick2014@yandex.by>
2025-09-12 19:27:21 +0300

    url

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, product_detail, cart_view
+from .views import index, product_detail, cart_view, cart_add
 
 app_name = 'product'
 
 urlpatterns = [
     path('', index, name='index'),
     path('cart/', cart_view, name='cart-view'),
+    path('add/', cart_add, name='add-to-cart'),
     path('<int:pk>/', product_detail, name='detail'),
 ]
